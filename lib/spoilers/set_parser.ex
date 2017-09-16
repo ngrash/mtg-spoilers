8e3d3cb93950d838d973283a8c8e5349397b69e9
Nico Grashoff <ngrash@gmail.com>
2017-09-17 01:27:11 +0200

    Make SetParser return correct URL and use regex
    The old way of parsing the HTML with Floki did not work with all set
    pages from http://mythicspoiler.com. We look for card URLs with a simple
    regular expression which works way better and yields more readable code.

--- lib/spoilers/set_parser.ex
+++ lib/spoilers/set_parser.ex
@@ -1,13 +1,19 @@
 defmodule Spoilers.SetParser do
-  def parse(html) do
-    {:ok, do_parse(html)}
+  def parse(html, set) do
+    {:ok, do_parse(html, set)}
   end
 
-  defp do_parse(html) do
-    tree = Floki.find(html, ".card")
-    Enum.map(tree, fn {"a", [_, {"href", href}], [{"img", img, _}]} ->
-      {_, src} = Enum.find(img, fn x -> elem(x, 0) == "src" end)
-      %{name: Path.basename(src, ".jpg"), href: href, img: src}
+  defp do_parse(html, set) when is_atom(set) do
+    do_parse(html, Atom.to_string(set))
+  end
+  defp do_parse(html, set) when is_binary(set) do
+    Regex.scan(~r/"cards\/(.+)\.html"/, html)
+    |> Enum.map(fn [_, name] ->
+      %{
+        name: name,
+        href: set <> "/cards/" <> name <> ".html",
+        img:  set <> "/cards/" <> name <> ".jpg"
+      }
     end)
   end
 end
--- test/spoilers/set_parser_test.exs
+++ test/spoilers/set_parser_test.exs
@@ -4,14 +4,14 @@ defmodule Spoilers.SetParserTest do
 
   test "parses set html" do
     html = Fixtures.set_html
-    {:ok, set} = Spoilers.SetParser.parse(html)
+    {:ok, set} = Spoilers.SetParser.parse(html, :ixa)
 
     # check some random samples
     sample = Enum.at(set, 0)
     name = sample[:name]
     assert sample == %{
-      href: "cards/" <> name <> ".html",
-      img: "cards/" <> name <> ".jpg",
+      href: "ixa/cards/" <> name <> ".html",
+      img:  "ixa/cards/" <> name <> ".jpg",
       name: name
     }
   end
